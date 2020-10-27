cc640af0d34b1f8880a5ada2beb41a2b922889e8
joao vian <joaovian@MacBook-Pro-de-joao.local>
2020-10-27 19:18:53 -0300

    treatment_types for select on treatment helper

--- app/helpers/treatments_helper.rb
+++ app/helpers/treatments_helper.rb
@@ -1,2 +1,7 @@
 module TreatmentsHelper
+  def treatment_types_for_select
+    Treatment.treatment_types.map do |k, v|
+      [Treatment.human_attribute_name("treatment_type.#{k}"), k]
+    end
+  end
 end
--- spec/helpers/treatments_helper_spec.rb
+++ spec/helpers/treatments_helper_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TreatmentsHelper. For example:
-#
-# describe TreatmentsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TreatmentsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'treatment types for select' do
+    it do
+      expect(treatment_types_for_select).
+        to eq(Treatment.treatment_types.map do
+          |k, v| [Treatment.human_attribute_name("treatment_type.#{k}"), k]
+        end)
+    end
+  end
 end
