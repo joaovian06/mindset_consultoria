d549581757fe04f6f7dc7d1f71b126e40ad5a5b1
joao vian <joaovian@MacBook-Pro-de-joao.local>
2020-10-28 00:41:15 -0300

    adjust treatments factory

--- spec/factories/treatments.rb
+++ spec/factories/treatments.rb
@@ -2,10 +2,7 @@ FactoryBot.define do
   factory :treatment do
     title { Faker::Company.catch_phrase }
     treatment_type { 1 }
-    status { 1 }
-    rate { 5 }
     description { "A default description. Please describe the treatment." }
-    budget { 0 }
     customer
     consultant
 
