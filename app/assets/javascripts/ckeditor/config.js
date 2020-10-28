40f9ff12d129c3372a37bb1c3ade91f1a2d10ae7
joao vian <joaovian@MacBook-Pro-de-joao.local>
2020-10-28 00:39:01 -0300

    config ckeditor on config.js

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -5,6 +5,7 @@ CKEDITOR.editorConfig = function (config) {
     ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
   ];
   config.toolbar = "mini";
+  config.autoParagraph = false;
 
   // ... rest of the original config.js  ...
 }
